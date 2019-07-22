e1dfa76661cc7f5867d65d091637399bf97e69fa
Somnath B <bhattacharya.som@gmail.com>
2019-07-21 23:20:08 -0400

    Restructuring.

--- java-features/src/main/java/com/somnath/apps/Person.java
+++
@@ -1,8 +0,0 @@
-package com.somnath.apps;
-
-/**
- * Created by somnath on 3/4/17.
- */
-public abstract class Person {
-
-}
--- java-features/src/main/java/com/somnath/apps/generics/Shape.java
+++
@@ -1,9 +0,0 @@
-package com.somnath.apps.generics;
-
-/**
- * Created by somnath on 3/4/17.
- */
-public interface Shape {
-
-    void draw();
-}
